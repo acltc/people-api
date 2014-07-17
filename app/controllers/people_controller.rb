a7f2191796a9341e3b2d951c6252ee790934fa80
Jay Wengrow <jaywngrw@gmail.com>
2014-07-17 17:53:11 -0500

    added create, update, and destroy

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,16 +1,40 @@
 class PeopleController < ApplicationController
+  respond_to :json, :xml
 
   def index
     @people = Person.all
-    render :json => @people
+    respond_with(@people)
   end
 
   def show
     @person = Person.find_by(:id => params[:id])
     if @person
-      render :json => @person
+      respond_with(@person)
     else
-      render :json => "Person not found", :status => 404
+      respond_with({message: 'Person not found'}, status: 404)
     end
+  end
+
+  def create
+    @person = Person.create(person_params)
+    respond_with(@person)
+  end
+
+  def update
+    @person = Person.find_by(:id => params[:id])
+    @person.update(person_params)
+    respond_with(@person)
+  end
+
+  def destroy
+    @person = Person.find_by(:id => params[:id])
+    @person.destroy if @person
+    respond_with(@person)
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:prefix, :first_name, :middle_name, :last_name, :suffix, :email, :phone)
   end
 end
