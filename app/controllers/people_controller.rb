047da0583fffbc5e8e5a9622d7ad55931cf187c2
Jay Wengrow <jaywngrw@gmail.com>
2014-07-21 18:49:18 -0500

    added update and destroy actions

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -15,6 +15,22 @@ class PeopleController < ApplicationController
     respond_with(@person)
   end
 
+  def update
+    @person = Person.find_by(:id => params[:id])
+    @person.update(person_params)
+    respond_with(@person)
+    # respond_to do |format|
+    #   format.json { render :json => @person }
+    #   format.xml { render :xml => @person }
+    # end
+  end
+
+  def destroy
+    @person = Person.find_by(:id => params[:id])
+    @person.destroy
+    respond_with(@person)
+  end
+
   private
 
   def person_params
