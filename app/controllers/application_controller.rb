bcd136bb7f0874a3e8a02f263328af849bca451a
Jay Wengrow <jaywngrw@gmail.com>
2014-07-29 12:02:32 -0500

    adding basic api_key auth to api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
   respond_to :json, :xml
+
+  before_filter :restrict_access
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      User.find_by(:email => request.headers["X-User-Email"], :api_key => token)
+      # api_key = ApiKey.find_by(access_token: token) 
+      # user = api_key && User.find_by(id: api_key.user_id)
+      # user && user.email == request.headers["X-User-Email"]
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717211408) do
+ActiveRecord::Schema.define(version: 20140729165325) do
 
   create_table "people", force: true do |t|
     t.string   "prefix"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20140717211408) do
     t.string   "suffix"
     t.string   "email"
     t.string   "phone"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "api_key"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
